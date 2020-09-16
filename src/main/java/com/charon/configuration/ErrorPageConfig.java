4d613ff2483fa9f6a812beee29f7d04a739a431f
Narcylix <narcylix@outlook.com>
2020-09-16 22:18:07 +0800

    配置默认错误页

---
+++ src/main/java/com/charon/configuration/ErrorPageConfig.java
@@ -0,0 +1,23 @@
+package com.charon.configuration;
+
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.ErrorPageRegistrar;
+import org.springframework.boot.web.server.ErrorPageRegistry;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+
+/**
+ * 默认错误页跳转配置
+ */
+@Configuration
+public class ErrorPageConfig implements ErrorPageRegistrar {
+    @Override
+    public void registerErrorPages(ErrorPageRegistry registry) {
+        //找不到页面
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/page/error/404.html");
+        //内部服务器错误
+        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/page/error/500.html");
+        //添加到配置中
+        registry.addErrorPages(errorPage404, errorPage500);
+    }
+}
