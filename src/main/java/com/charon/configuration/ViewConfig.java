8eff8b75692cc8e5d950ea4b82b635f628054d05
snow <snowiests@outlook.com>
2020-09-19 21:22:10 +0800

    新增：视图解析器 移除：thymeleaf模板引擎（前后端分离）

---
+++ src/main/java/com/charon/configuration/ViewConfig.java
@@ -0,0 +1,16 @@
+package com.charon.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 视图解析
+ */
+@Configuration
+public class ViewConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/index").setViewName("/views/index.html");
+    }
+}
