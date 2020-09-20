7f26d12880c8d729ec42c45ee0967a7fceec5d84
snow <snowiests@outlook.com>
2020-09-20 10:37:32 +0800

    完善视频分类管理的前后端业务逻辑

--- src/main/java/com/charon/configuration/InterceptorConfig.java
+++ src/main/java/com/charon/configuration/InterceptorConfig.java
@@ -12,6 +12,6 @@ public class InterceptorConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new TestHandlerInterceptor())
                 .addPathPatterns("/**")
-                .excludePathPatterns("/**/*.js", "/**/*.css");
+                .excludePathPatterns("/**/*.js", "/**/*.css", "/**/font/**");
     }
 }
